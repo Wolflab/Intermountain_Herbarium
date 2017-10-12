a45e3be43b67195b7caae3bf89bede7d0cef1e53
Ronin11 <nate.ashby11@gmail.com>
2017-10-12 09:56:08 -0600

    Server dev is annoying af

--- models/reports.js
+++ models/reports.js
@@ -92,10 +92,9 @@ exports.getDatabaseDataset = function(){
 exports.getTotalDataset = async function(){
 	var docs = await db.reports.find();
 	var temp = docs.map(function(val, index, arr){
-		console.log(JSON.stringify(val.report, null, 4))
 		return {
 			"fiscalYear": val.fiscalYear,
-			"specimenTotal": val.report.specimen.activities.totals.data.specimenTotal.inputs.value
+			"specimenTotal": val.report.specimen.activities.totals.data.specimenTotal.inputs.count.value
 		}
 	});
 	console.log(temp);
